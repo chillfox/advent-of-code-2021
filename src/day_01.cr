1c545e28e45a531b48860753a92434bcfb3cbb65
Mia Bennett <chillfox@hey.com>
2021-12-02 00:53:46 +0000

    Challenge two complete

--- src/day_01.cr
+++ src/day_01.cr
@@ -1,5 +1,14 @@
 path = "./data/day_01_test.txt" unless path = ARGV.first?
 
+###################
+# First Challenge #
+###################
+
+puts "\n\n"
+puts "First Challenge"
+puts "########################################"
+puts "\n"
+
 last_depth = nil
 increases = 0
 
@@ -21,3 +30,37 @@ File.each_line(path) do |line|
 end
 
 puts "Depth increased #{increases} times"
+
+####################
+# Second Challenge #
+####################
+
+puts "\n\n"
+puts "Second Challenge"
+puts "########################################"
+puts "\n"
+
+window = [] of Int64
+increases = 0
+
+File.each_line(path) do |line|
+  depth = line.to_i64
+  window << depth
+  window.shift if window.size > 4
+
+
+  if window.size == 4
+    if window[0,3].sum < window[1,4].sum
+      puts "#{window[1,4].sum} (increased)"
+      increases += 1
+    elsif window[0,3].sum > window[1,4].sum
+      puts "#{window[1,4].sum} (decreased)"
+    else
+      puts "#{window[1,4].sum} (no change)"
+    end
+  elsif window.size == 3
+    puts "#{window.sum} (N/A - no previous sum)"
+  end
+end
+
+puts "Sum increased #{increases} times"
