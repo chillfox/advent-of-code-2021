7afcba2c9972ac166bb6047b31f76e6d3d511fc6
Mia Bennett <chillfox@hey.com>
2021-12-07 07:45:00 +0000

    day 7 challenge 2

--- src/day_07.cr
+++ src/day_07.cr
@@ -18,3 +18,34 @@ crabs.each do |crab|
 end
 
 puts "Total fuel cost: #{fuel}" # 37
+
+####################
+# Second Challenge #
+####################
+
+puts "\n\n"
+puts "Second Challenge"
+puts "########################################"
+puts "\n"
+
+# crabs.sum / crabs.size
+# 4.9 on test data (correct: 5)
+# 486.505 on full data set (correct: 486)
+
+def round(num : Float) : Int
+  n = num.round(1, mode: :ties_away)
+  if n % 1 <= 0.5
+    (n // 1).to_i
+  else
+    n.round.to_i
+  end
+end
+
+align_at = round(crabs.sum / crabs.size)
+fuel = 0
+crabs.each do |crab|
+  steps = (crab - align_at).abs
+  fuel += (steps * (steps + 1) / 2).to_i
+end
+
+puts "Total fuel cost: #{fuel}" # 168
